f01c22bcb08f399c5d3ef440c97e33087a219cca
thanhhangdev <thanhhangdev@hotmail.com>
2018-07-10 16:16:10 +0700

    Changr detail

--- src/index.js
+++ src/index.js
@@ -5,19 +5,16 @@ import App from './pages/Home';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
-{/*function RenderApp(req) {
-  return (<App/>);
-}*/}
+import Index from './pages/Detail/index';
 
 
 ReactDOM.render(
   <BrowserRouter>
     <React.Fragment>
       <Switch>
-        <Route exact path="/" component={App}/>
+      <Route exact path="/" component={App}/>
         <Route exact path="/home" component={App}/>
-        <Route exact path="/about" component={App}/>
+        <Route exact path="/detail/:id" component={Index} />
         <Route exact path="/:page" component={App}/>
         <Route exact path="/home/:page" component={App}/>
       </Switch>
