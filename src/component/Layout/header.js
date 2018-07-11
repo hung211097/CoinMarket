6fd90b8e4eee4f2893167bbb1a3ee916677126af
hung211097 <1512227@student.hcmus.edu.vn>
2018-07-11 11:45:45 +0700

    Update Header for show "(page x)" at title in Homepage when page > 1

--- src/component/Layout/header.js
+++ src/component/Layout/header.js
@@ -2,10 +2,29 @@ import React from 'react';
 import {number} from '../../constant/config';
 
 class header extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      page: this.props.page,
+    }
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState)
+  {
+      if(nextProps.page !== prevState.page)
+      {
+        return{
+          page: nextProps.page,
+        }
+      }
+      return null;
+  }
+
   render(){
     return(
       <div className="titleContain">
-        <h2 className="title">Top {number.NUMBER_ITEMS} Cryptocurrencies by Market Capitalization</h2>
+        <h2 className="title">Top {number.NUMBER_ITEMS} Cryptocurrencies by Market Capitalization {this.state.page > 1 && `(page ${this.state.page})`}</h2>
         <hr/><br/><br/>
       </div>
     );
